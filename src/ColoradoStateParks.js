1652ab6c3784d3eb5d8e8dd189fa632bd0501ea5
Kristina Diaz <kristina.diaz84@gmail.com>
2021-11-22 13:37:49 -0700

    completed part of code along

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,11 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
+  MesaVerde();
 
   return <h1>Colorado State Parks!</h1>;
 }
